c1c8a8d9045d78c874083b81c28a84dbc02fd60b
Alan Hortz <alan.hortz@gmail.com>
2017-02-09 12:05:00 +0100

    add stickertag

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ var log = new Logger({
   token:'fb3ab3f2-d8c6-4e77-8ce2-75962936fecc'
 });
 
+const stickersMap = {
+    'b5018c2fff14ae40':'front-door'
+    'a4011c8c1bccdf55':'office-door'};
 
 EstimoteSticker.on('discover', function(estimoteSticker) {
+    estimoteSticker.tag = stickersMap[estimoteSticker.id];
     log.log("info",estimoteSticker);
 });
 

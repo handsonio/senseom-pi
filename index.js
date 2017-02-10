6a1d8feec4dd743e118c330ca820c752da241a13
Alan Hortz <alan.hortz@gmail.com>
2017-02-10 10:29:01 +0100

    add other stickers tag name

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var log = new Logger({
 
 const stickersMap = {
     'b5018c2fff14ae40':'front-door',
-    'a4011c8c1bccdf55':'office-door'};
+    'a4011c8c1bccdf55':'office-door',
+    '486a1154d89e3862':'wc-door',
+    '58ec63d39087614e':'fridge-door'};
 
 EstimoteSticker.on('discover', function(estimoteSticker) {
     estimoteSticker.tag = stickersMap[estimoteSticker.id];

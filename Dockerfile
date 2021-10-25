5d4e4d8e08a31872aac09dba39df7f4d0113e043
Alan Hortz <alan@handson.io>
2021-10-26 01:44:22 +0200

    ...

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM balenalib/raspberrypi3-node:12-latest-build as build
 
-WORKDIR /usr/src/app
+WORKDIR /app
 
 #install some dependencies for noble and balena-sdk
 RUN install_packages make g++ bluetooth bluez libbluetooth-dev libudev-dev git
@@ -9,65 +9,15 @@ COPY . .
 
 #install node dependencies
 RUN npm install @abandonware/noble
-RUN npm install express
+RUN npm install node-beacon-scanner
 RUN JOBS=MAX npm install --production --unsafe-perm && npm cache clean --force && rm -rf /tmp/*
 
 FROM balenalib/raspberrypi3-node:12-latest-run
 
-WORKDIR /usr/src/app
+#Set permissions to allow BLE scanning
+RUN sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
 
-
-ENV INITSYSTEM on
-
-
-# use apt-get if you need to install dependencies
-#RUN apt-get update
-#RUN apt-get install wget
-
-
-
-# add the key for foundation repository
-#RUN wget http://archive.raspberrypi.org/debian/raspberrypi.gpg.key -O - | sudo apt-key add -
-
-# add apt source of the foundation repository
-# we need this source because bluez needs to be patched in order to work with RPi3
-# issue #1314: How to get BT working on Pi3B. by clivem in raspberrypi/linux on GitHub
-#RUN sed -i '1s#^#deb http://archive.raspberrypi.org/debian jessie main\n#' /etc/apt/sources.list
-
-
-
-
-
-
-# Install openSSH, remove the apt list to reduce the size of the image
-#RUN apt-get update && apt-get install  \
-#     bluetooth bluez  \
-#     libbluetooth-dev libudev-dev python3 python3-pip make g++
-
-
-# Copy Storage mount definition file for systemd
-#COPY mounts/mnt-media.mount /lib/systemd/system
-
-#COPY package.json /usr/src/app/
-
-#RUN npm install
-
-
-# Copy all files from root to the WORKDIR
-
-#COPY . /usr/src/app
-
-
-# Enable Bluetooth on application start
-#RUN systemctl enable bb-wl18xx-bluetooth.service
-
-# Mount storage on application start
-#RUN systemctl enable mnt-media.mount
-
-
-#CMD ["bash", "start.sh"]
-COPY --from=build /usr/src/app /usr/src/app
+WORKDIR /app
+COPY --from=build /app /app
 
 CMD ["npm", "start"]
-
-
--- index.js
+++ index.js
@@ -1,61 +1,75 @@
-
-const stickersMap = {
-    'a4011c8c1bccdf55':'beacon1',
-    'a435b39904f64794':'beacon2',
-    'f9d4334332bb3af1':'beacon3',
-};
+const BeaconScanner = require('node-beacon-scanner');
+const scanner = new BeaconScanner();
 
 
-const stickerDatabase = {
-    'beacon1':null,
-    'beacon2':null,
-    'beacon3':null
+scanner.onadvertisement = (ad) => {
+  console.log(ad.id + ' / ' + ad.beaconType)
 }
 
-
-var Bleacon = require('bleacon');
-var EstimoteSticker = Bleacon.EstimoteSticker;
-
-var app = require('express')();
-var server = require('http').Server(app);
-//var io = require('socket.io')(server);
-
-server.listen(80);
-
-app.get('/', function (req, res) {
-  res.status(200).json(['beacon1','beacon2','beacon3'])
+// Start scanning for iBeacons
+scanner.startScan().then(() => {
+  console.log('Started to scan.');
+}).catch((error) => {
+  console.error(error);
 });
 
-app.get('/beacon1', function (req, res) {
-  res.status(200).json(stickerDatabase['beacon1'])
-})
-
-app.get('/beacon2', function (req, res) {
-  res.status(200).json(stickerDatabase['beacon2'])
-})
-
-app.get('/beacon3', function (req, res) {
-  res.status(200).json(stickerDatabase['beacon3'])
-})
+// const stickersMap = {
+//     'a4011c8c1bccdf55':'beacon1',
+//     'a435b39904f64794':'beacon2',
+//     'f9d4334332bb3af1':'beacon3',
+// };
 
 
-EstimoteSticker.on('discover', function(estimoteSticker) {
-    
-    estimoteSticker.tag = stickersMap[estimoteSticker.id];
-
-    stickerDatabase[stickersMap[estimoteSticker.id]] = estimoteSticker
-    //publishBeaconAdvertisement(estimoteSticker);
-  //  io.emit('sticker', estimoteSticker);
-
-});
-
-EstimoteSticker.startScanning();
-
-// function publishBeaconAdvertisement(sticker) {
-//   publishConfig.message = sticker;
-//   pubnub.publish(publishConfig, function(status, response) {
-//       if(status.error === true) {
-//         console.log(status, response);
-//       }
-//   });
+// const stickerDatabase = {
+//     'beacon1':null,
+//     'beacon2':null,
+//     'beacon3':null
 // }
+
+
+// var Bleacon = require('bleacon');
+// var EstimoteSticker = Bleacon.EstimoteSticker;
+
+// var app = require('express')();
+// var server = require('http').Server(app);
+// //var io = require('socket.io')(server);
+
+// server.listen(80);
+
+// app.get('/', function (req, res) {
+//   res.status(200).json(['beacon1','beacon2','beacon3'])
+// });
+
+// app.get('/beacon1', function (req, res) {
+//   res.status(200).json(stickerDatabase['beacon1'])
+// })
+
+// app.get('/beacon2', function (req, res) {
+//   res.status(200).json(stickerDatabase['beacon2'])
+// })
+
+// app.get('/beacon3', function (req, res) {
+//   res.status(200).json(stickerDatabase['beacon3'])
+// })
+
+
+// EstimoteSticker.on('discover', function(estimoteSticker) {
+    
+//     estimoteSticker.tag = stickersMap[estimoteSticker.id];
+
+//     stickerDatabase[stickersMap[estimoteSticker.id]] = estimoteSticker
+//     //publishBeaconAdvertisement(estimoteSticker);
+//   //  io.emit('sticker', estimoteSticker);
+
+// });
+
+// EstimoteSticker.startScanning();
+
+// // function publishBeaconAdvertisement(sticker) {
+// //   publishConfig.message = sticker;
+// //   pubnub.publish(publishConfig, function(status, response) {
+// //       if(status.error === true) {
+// //         console.log(status, response);
+// //       }
+// //   });
+// // }
